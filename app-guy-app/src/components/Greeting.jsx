cebb03ce2d22db01ecdb216dc9c5c21e01351810
Suwaphat Watthakicharon <watthakicharoen_s@su.ac.th>
2025-08-28 05:07:52 +0000

    components app

---
+++ app-guy-app/src/components/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Greeting = () => {
+    return (
+        <div className="greeting">
+            <h2>สวัสดี React Developer!</h2>
+            <p>มาสร้างสิ่งที่น่าทึ่งด้วยกัน</p>
+        </div>
+    );
+};
+
+export default Greeting;
